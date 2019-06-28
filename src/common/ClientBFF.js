92babbc3fbec2841d5fde343f6245930ff449d2a
Cristian <cristian.tejadan@gmail.com>
2019-06-28 17:57:09 -0400

    fix in bff client

--- src/common/ClientBFF.js
+++ src/common/ClientBFF.js
@@ -11,48 +11,51 @@ export default class ClientAPI {
 
   getInstance = () => {
     const bffInstance = axios.create({
-      baseURL: 'https://ripley-bff.herokuapp.com/',
+      baseURL: process.env === 'production'? 'https://ripley-bff.herokuapp.com/' : 'http://localhost:3000',
       timeout: 5000,
     })
     axiosRetry(bffInstance, retryConfig)
     return bffInstance
   }
 
-  getWeather = async (lat, lng) => {
-    return new Promise((resolve, reject) => {
 
+  handleSimulatedError = (lat, lng, resolve) => {
+
+    const simulatedError = setInterval(() => {
       if (Math.random() > 0.1) {
         this.getInstance().request({
           url: 'weather',
           method: 'get',
           params: { lat, lng }
         }).then(response => {
-          clearInterval(answered)
           resolve(response.data)
+          clearInterval(simulatedError)
         })
         .catch(error => console.log(error.message))
       } else {
         console.log('random error')
       }
+    }, 5000)
 
-      const answered = setInterval(() => {
-        if (Math.random() > 0.1) {
-          this.getInstance().request({
-            url: 'weather',
-            method: 'get',
-            params: { lat, lng }
-          }).then(response => {
-            clearInterval(answered)
-            resolve(response.data)
-          })
-          .catch(error => console.log(error.message))
-        } else {
-          console.log('random error')
-        }
-      }, 5000)
+    if (Math.random() > 0.1) {
+      this.getInstance().request({
+        url: 'weather',
+        method: 'get',
+        params: { lat, lng }
+      }).then(response => {
+        resolve(response.data)
+        clearInterval(simulatedError)
+      })
+      .catch(error => console.log(error.message))
+    } else {
+      console.log('random error')
+    }
+  }
 
+  getWeather = async (lat, lng) => {
+    return new Promise((resolve, reject) => {
+      this.handleSimulatedError(lat, lng, resolve)
     })
-
   }
 
   getCoordinatesByCity = async (city) => {
